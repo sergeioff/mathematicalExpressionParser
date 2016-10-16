6ed296e9816b052e09bbf0caf3561e4466fedb8d
SergeiOFF <pogorelov.s.y@gmail.com>
2016-10-16 14:57:30 +0300

    add ^ to operations

--- parser/src/main/java/com/github/sergeioff/mathematicalExpressionParser/parser/Parser.java
+++ parser/src/main/java/com/github/sergeioff/mathematicalExpressionParser/parser/Parser.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
  */
 public class Parser {
     private final Pattern validCharsPattern = Pattern.compile("[^0-9+-/*.(){}\\[\\]%\\^ ]");
-    private final String[] OPERATORS = {"+", "-", "*", "/", "%"};
+    private final String[] OPERATORS = {"+", "-", "*", "/", "%", "^"};
     private String line;
     private StringBuilder lineToParse;
 
